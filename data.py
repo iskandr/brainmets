3cb27d8ff3182683eecb3256a2651da9058087c6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-06-02 20:21:44 -0400

    try different scikit learn + feature engineering

--- data.py
+++ data.py
@@ -1,30 +1,103 @@
-
+import pandas as pd
+import numpy as np
+import sklearn.preprocessing
 
 FILENAME = 'BrainMets.xlsx'
 MONTHS_TO_LIVE = 9
 N_TRAIN = 250
 
-def make_dataset(df, months_to_live = MONTHS_TO_LIVE):
-	mask = (df['Dead'] | (df['SurvivalMonths'] >= months_to_live))
-	y = np.array((df['Dead'] & (df['SurvivalMonths'] < months_to_live)).ix[mask] == 1)
-	Xa = np.array(X.ix[mask]).astype('int')
-	return Xa, y
+def make_dataset(df, binarize_categorical):
+	"""
+	Load dataset with continuous outputs
+	"""
+	df = df.copy()
+	df['cancer type']  = df['cancer type'].str.lower()
+	cancer_types = df['cancer type'].unique()
+	cancer_type_col = np.zeros(len(df['cancer type']), dtype=int)
+	for (i, cancer_type) in enumerate(cancer_types):
+		cancer_type_col[np.array(df['cancer type'] == cancer_type)] = i
+	df['cancer type'] = cancer_type_col
+	df['# of tumors > 1'] = df['# of tumors'] > 1
+	df['age 30s'] = (df['age'] >= 30) & (df['age'] < 40)
+	df['age 40s'] = (df['age'] >= 40) & (df['age'] < 50)
+	df['age 50s'] = (df['age'] >= 50) & (df['age'] < 60)
+	df['age 60s'] = (df['age'] >= 60) & (df['age'] < 70)
+	df['age 70s'] = (df['age'] >= 70) & (df['age'] < 80)
+	df['age 80s'] = df['age'] >= 80
+
+	fields = [ 
+		'# of tumors > 1', 
+		#'age',
+		'age 30s',
+		'age 40s',
+		'age 50s',
+		'age 60s',
+		'age 70s',
+		'age 80s',
+		'cancer type', 
+		'ECOG', 
+		'Prior WBRT', 
+		'Brain Tumor Sx', 
+		'RPA', 
+		'Diagnosis of Primary at the same time as Brain tumor'
+	]
+	X_raw = df[fields]
+	X = np.array(X_raw).astype('int')
+	if binarize_categorical:
+		n_features = X.shape[1]
+		binarize_mask = np.ones(n_features, dtype=bool)
+		# age 
+		binarize_mask[0:7] = False
+		# number of tumors 
+		#binarize_mask[7] = False
+		
+		binarizer = sklearn.preprocessing.OneHotEncoder(categorical_features = binarize_mask)
+		X = binarizer.fit_transform(X).todense()
+		print np.sum(X[:, -1])
+	Y = np.array(df['SurvivalMonths'])
+	expert_predictions = {}
+	experts = [
+		'Prediction(Cleveland Clinic)', 
+		' Prediction (Lanie Francis)', 
+		'Prediction(Flickinger)', 
+		'Prediction(Loefler', 
+		'Prediction(Knisely)', 
+		'Prediction(Lunsford)', 
+		'Prediction (Tahrini)', 
+		'Prediction (Sheehan)', 
+		'Prediction (Linskey)', 
+		'Prediction(friedman)', 
+		'Prediction(Stupp)', 
+		'Prediction(Rakfal)', 
+		'Prediction(Rush)', 
+		' Prediction( Kondziolka)'
+	]
+	for expert in experts:
+		expert_predictions[expert] = df[expert]
+	dead = df['Dead']
+	return X, Y, dead, expert_predictions
+
+def make_labeled_dataset(df, months_to_live = MONTHS_TO_LIVE, binarize_categorical = True):
+	X, Y_continuous, dead, expert_predictions = make_dataset(df, binarize_categorical)
+	# get rid of patients for whom we don't have a long enough history
+	mask = np.array(dead | (Y_continuous >= months_to_live))
+	X = X[mask]
+	Y = dead[mask] & (Y_continuous[mask] < months_to_live)
+	return X, Y
 
 def load_dataframe(filename = FILENAME):
 	return pd.read_excel(filename, 'DATA', header=1)
 	
-def load_dataset(filename = FILENAME, months_to_live = MONTHS_TO_LIVE):
+def load_dataset(filename = FILENAME, binarize_categorical = True):
 	df = load_dataframe(filename)
-	return make_dataset(df, months_to_live)
+	return make_dataset(df, binarize_categorical = binarize_categorical)
 
-def split_dataset(df, months_to_live = MONTHS_TO_LIVE, n_train = N_TRAIN, binarize_categorical = True, shuffle = True, verbose = True):
-	X, y = make_dataset_arrays(df, months_to_live = months_to_live)
-	n_features = Xa.shape[1]
-	if binarize_categorical:
-		binarize_mask = np.ones(n_features, dtype=bool)
-		binarize_mask[0] = False
-		binarizer = sklearn.preprocessing.OneHotEncoder(categorical_features = binarize_mask)
-		X = binarizer.fit_transform(X).todense()
+def load_labeled_dataset(filename = FILENAME, months_to_live = MONTHS_TO_LIVE, binarize_categorical = True):
+	df = load_dataframe(filename)
+	return make_labeled_dataset(df, months_to_live, binarize_categorical = binarize_categorical)
+
+def split_labeled_dataset(df, months_to_live = MONTHS_TO_LIVE, n_train = N_TRAIN, binarize_categorical = True, shuffle = True, verbose = True):
+	X, y = make_labeled_dataset(df, months_to_live = months_to_live, binarize_categorical = binarize_categorical)
 	if shuffle:
 		idx = np.arange(len(y))
 		np.random.shuffle(idx)
---
+++ models.py
@@ -0,0 +1,96 @@
+import data 
+import numpy as np 
+import sklearn.linear_model
+import sklearn.naive_bayes 
+import sklearn.cross_validation
+import sklearn.ensemble
+import sklearn.decomposition
+
+                
+def load_datasets(months_to_live_start = 1, months_to_live_stop = 15, binarize_categorical = True):
+	Xs = {}
+	Ys = {}
+	for i in xrange(months_to_live_start, months_to_live_stop + 1):
+		print "Loading dataset for %d months to live" % i 
+		X, Y = data.load_dataset(months_to_live = i, binarize_categorical = binarize_categorical)
+		Xs[i] = X
+		Ys[i] = Y
+	return Xs, Ys
+"""
+def level_sets_to_continuous(predictors, Xs, Ys)
+	keys = list(sorted(Xs.keys()))
+	for i in keys:
+		left = ...
+		right = ... 	
+		optimize 1-mean(left) + mean(right)
+"""
+def binary_predictors(start = 3, stop = 20):
+	for i in xrange(start, stop):
+		X, Y = data.load_labeled_dataset(months_to_live = i, binarize_categorical = True)
+		model = sklearn.linear_model.LogisticRegression()
+		aucs = sklearn.cross_validation.cross_val_score(model, X, Y, cv = 10, scoring='roc_auc')
+		auc = np.mean(aucs)
+		print "Months to live = %d, n_dead = %d, ROC AUC = %0.4f" % (i, np.sum(Y), auc)
+
+
+if __name__ == '__main__':
+
+	X, Y, dead, experts = data.load_dataset(binarize_categorical = True)
+	X = np.array(X)
+	
+	print "Data shape", X.shape
+	test_set_mask = np.zeros_like(Y, dtype='bool')
+	
+	Y_expert_combined = np.zeros_like(Y)
+	Y_expert_count = np.zeros_like(Y, dtype=int)
+
+	for expert in experts:
+		Y_pred = experts[expert]
+		mask = np.array(~(Y_pred.isnull()))
+		test_set_mask |= mask
+		print expert.strip(), "n =", np.sum(mask)
+		# X_subset = X[mask]
+		Y_pred_subset = np.array(Y_pred[mask].astype('float'))
+		diff = Y[np.array(mask)] -  Y_pred_subset
+		error = np.mean(np.abs(diff))
+		print "-- %0.4f" % error
+		Y_expert_combined[mask] += Y_pred_subset
+		Y_expert_count[mask] += 1
+
+	combined_mask = Y_expert_count > 0
+	Y_expert_combined = Y_expert_combined[combined_mask]
+	Y_expert_combined /= Y_expert_count[combined_mask]
+	print "---"
+	print "Average prediction error = %0.4f" % (np.mean(np.abs(Y_expert_combined - Y[combined_mask])))
+	print "---"
+
+	n_test = np.sum(test_set_mask)
+	n_train = len(Y) - n_test
+	print "Training set = %d, test set = %d" % (n_train, n_test)
+
+	X_train = X[~test_set_mask]
+	Y_train = Y[~test_set_mask]
+
+	X_test = X[test_set_mask]
+	Y_test = Y[test_set_mask]
+
+
+	pca = sklearn.decomposition.PCA(10)
+	X_train = pca.fit_transform(X_train)
+	print "PCA transformed shape", X_train.shape
+	X_test = pca.transform(X_test)
+
+
+	def fit(model):
+		model.fit(X_train, Y_train)
+		pred = model.predict(X_test)
+		print "%s error: %0.4f" % (model.__class__.__name__, np.mean(np.abs(pred - Y_test)))
+
+	fit(sklearn.linear_model.LinearRegression())
+	fit(sklearn.svm.SVR())
+	fit(sklearn.linear_model.RidgeCV(alphas = [0.001, 0.01, 0.1, 1, 10, 100, 1000]))
+	fit(sklearn.linear_model.LassoCV())
+	fit(sklearn.linear_model.OrthogonalMatchingPursuitCV())
+	fit(sklearn.ensemble.RandomForestRegressor(n_estimators = 500))
+
+
